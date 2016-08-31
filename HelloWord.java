79b6faa67b0acdb282cb7dd34fc9213b78f0921b
sivashankar4s <sekarsivashankar4@gmail.com>
2016-08-31 17:22:18 +0530

    two lines add to hello world

--- HelloWord.java
+++ HelloWord.java
@@ -12,6 +12,9 @@ public class HelloWorld {
     public static void main(String[] args) {
         // Prints "Hello, World" in the terminal window.
         System.out.println("Hello, World I am Ashok"); 
+         System.out.println("Hello, World I am Ashok"); 
+          System.out.println("Hello, World I am rajjjjjjjjj");  
+          System.out.println("Hello, World I am ssssssss"); 
     }
 }
 
