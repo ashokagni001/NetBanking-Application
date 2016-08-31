04c09d72899e4b074edcc60e19810d60f3968ba5
ashokagni <ashokagni001@gmail.com>
2016-08-31 16:43:14 +0530

    ADD HelloWord and sample file

---
+++ sample.java
@@ -0,0 +1,17 @@
+/**
+ * <p>
+ * Simple java program.
+ * It say to Hello, World
+ * </p>
+ * 
+ * @author Ashok
+ * 
+ * @created 2016-08-27 
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        // Prints "Hello, World" in the terminal window.
+        System.out.println("Hello, World"); 
+    }
+}
+
