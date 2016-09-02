8195e15586309488f60f9b4734878e27c6232fd1
ashokagni <ashokagni001@gmail.com>
2016-09-02 10:00:36 +0530

    model class user and pojo

---
+++ model/sample.java
@@ -0,0 +1,21 @@
+/**
+ * <p>
+ * Simple java program.
+ * It say to Hello, World
+ * </p>
+ * 
+ * @author Ashok
+ * 
+ * @created 2016-08-27 
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        // Prints "Hello, World" in the terminal window.
+<<<<<<< Updated upstream
+        System.out.println("Hello World"); 
+=======
+        System.out.println("Hello, World"); 
+>>>>>>> Stashed changes
+    }
+}
+
