a9f6ee57acc8aeabea88fc0afd834eba248a1d44
ashokagni <ashokagni001@gmail.com>
2016-09-02 09:48:38 +0530

    model class role and user

---
+++ Role.java
@@ -0,0 +1,28 @@
+package com.i2i.netbankingApplication.model;
+
+public class Role {
+	private int id;
+	private String role;
+	
+	public Role() {
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+}
+	
+
+
